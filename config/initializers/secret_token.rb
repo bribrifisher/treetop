b162953d823811e53095405043e5123128a28c47
Brian Fisher <BrianFisher@brians-mbp.home>
2014-05-17 12:27:13 -0400

    Created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treetop::Application.config.secret_key_base = 'df6236173445ef0265acca0904dff77bd59ecf75b9885914149ec8d40281c698dbcd9697ec741e230bea3abef1414db9d6d050fca6e76cb02ba56271b3ec8d61'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Treetop::Application.config.session_store :cookie_store, key: '_treetop_session'
